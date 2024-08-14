d4e3ce2de9704f51d23ddc50e4e9160a6c8d7116
Alessandro Bartoli <alessandro@bartolilab.it>
2024-08-14 22:09:57 +0200

    fix: restored `discipline.lua`

--- lua/alle/discipline.lua
+++ lua/alle/discipline.lua
@@ -6,7 +6,7 @@ function M.cowboy()
    local id
    local ok = true
 
-   for _, key in ipairs({ "h", "j", "k", "l", "+", "-", "<Left>", "<Right>" }) do
+   for _, key in ipairs({ "h", "j", "k", "l", "+", "-", "<Up>", "<Down>", "<Left>", "<Right>" }) do
       local count = 0
       local timer = assert(vim.uv.new_timer())
       local map = key
