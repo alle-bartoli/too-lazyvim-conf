51842fac732bdb4d08f486c026fb6d4ec933d227
Alessandro Bartoli <alessandro@bartolilab.it>
2025-04-14 11:20:47 +0200

    installed `telescope` plugin

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,15 +2,27 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.mouse = "a" -- Enable mouse. (Default "")
+-- Enable mouse. (Default "")
+vim.opt.mouse = "a"
+
 vim.opt.title = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
-vim.opt.path:append({ "**" }) -- Finding files - Search down into subfolders
+
+-- Finding files - Search down into subfolders
+vim.opt.path:append({ "**" })
 vim.opt.wildignore:append({ "*/node_modules/*" })
-vim.g.lazyvim_picker = "fzf" -- Required for fzf.
-vim.g.lazyvim_python_lsp = "pyright" -- LSP Server to use for Python. Set to "basedpyright" to use basedpyright instead of pyright.
-vim.g.lazyvim_python_ruff = "ruff" -- Set to "ruff_lsp" to use the old LSP implementation version.
+
+-- Required for fzf
+vim.g.lazyvim_picker = "fzf"
+
+-- LSP Server to use for Python. Set to "basedpyright" to use basedpyright instead of pyright
+vim.g.lazyvim_python_lsp = "pyright"
+-- Set to "ruff_lsp" to use the old LSP implementation version
+vim.g.lazyvim_python_ruff = "ruff"
+
+-- Enable telescope
+vim.g.lazyvim_picker = "telescope"
 
 -- set to `true` to follow the main branch
 -- you need to have a working rust toolchain to build the plugin in this case.
