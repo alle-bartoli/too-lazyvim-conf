863bfd6cb5c8c857122383fb32c8b4e2f3452ff3
Alessandro Bartoli <alessandro@bartolilab.it>
2025-05-14 09:22:46 +0200

    Rollback to default config of `blink.cmp` plugin due to problems - updated plugins

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -18,92 +18,75 @@ return {
    },
 
    -- Refactoring tool
-   -- {
-   --   "ThePrimeagen/refactoring.nvim",
-   --    keys = {
-   --      {
-   --        "<leader>r",
-   --      function()
-   --       require("refactoring").select_refactor()
-   --     end,
-   --  mode = "v",
-   --  noremap = true,
-   --  silent = true,
-   --  expr = false,
-   -- },
-   -- },
-   -- opts = {},
-   -- },
+   {
+      "ThePrimeagen/refactoring.nvim",
+      keys = {
+         {
+            "<leader>r",
+            function()
+               require("refactoring").select_refactor()
+            end,
+            mode = "v",
+            noremap = true,
+            silent = true,
+            expr = false,
+         },
+      },
+      opts = {},
+   },
 
    {
       "saghen/blink.cmp",
+
+      -- optional: provides snippets for the snippet source
+      dependencies = "rafamadriz/friendly-snippets",
+
+      -- use a release tag to download pre-built binaries
+      version = "*",
+      -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+      -- build = 'cargo build --release',
+      -- If you use nix, you can build from source using latest nightly rust with:
+      -- build = 'nix run .#build-plugin',
+
+      ---@module 'blink.cmp'
+      ---@type blink.cmp.Config
       opts = {
-         snippets = {
-            expand = function(snippet, _)
-               return LazyVim.cmp.expand(snippet)
-            end,
-         },
+         -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept, C-n/C-p for up/down)
+         -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys for up/down)
+         -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+         --
+         -- All presets have the following mappings:
+         -- C-space: Open menu or open docs if already open
+         -- C-e: Hide menu
+         -- C-k: Toggle signature help
+         --
+         -- See the full "keymap" documentation for information on defining your own keymap.
+         keymap = { preset = "default" },
+
          appearance = {
-            -- sets the fallback highlight groups to nvim-cmp's highlight groups
-            -- useful for when your theme doesn't support blink.cmp
-            -- will be removed in a future release, assuming themes add support
-            use_nvim_cmp_as_default = false,
-            -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-            -- adjusts spacing to ensure icons are aligned
+            -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+            -- Useful for when your theme doesn't support blink.cmp
+            -- Will be removed in a future release
+            use_nvim_cmp_as_default = true,
+            -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+            -- Adjusts spacing to ensure icons are aligned
             nerd_font_variant = "mono",
          },
-         completion = {
-            accept = {
-               -- experimental auto-brackets support
-               auto_brackets = {
-                  enabled = true,
-               },
-            },
-            menu = {
-               draw = {
-                  treesitter = { "lsp" },
-               },
-            },
-            documentation = {
-               auto_show = true,
-               auto_show_delay_ms = 200,
-            },
-            ghost_text = {
-               enabled = vim.g.ai_cmp,
-            },
-         },
 
-         -- experimental signature help support
-         -- signature = { enabled = true },
-
+         -- Default list of enabled providers defined so that you can extend it
+         -- elsewhere in your config, without redefining it, due to `opts_extend`
          sources = {
-            -- adding any nvim-cmp sources here will enable them
-            -- with blink.compat
-            compat = {},
-            cmdline = {},
-            -- add lazydev to your completion providers
-            default = { "lazydev" }, -- default = { "lsp", "path", "snippets", "buffer" },
-            providers = {
-               lazydev = {
-                  name = "LazyDev",
-                  module = "lazydev.integrations.blink",
-                  score_offset = 100, -- show at a higher priority than lsp
-               },
-            },
+            default = { "lsp", "path", "snippets", "buffer" },
          },
 
-         keymap = {
-            preset = "enter",
-            ["<C-y>"] = { "select_and_accept" },
-         },
+         -- Blink.cmp uses a Rust fuzzy matcher by default for typo resistance and significantly better performance
+         -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+         -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+         --
+         -- See the fuzzy documentation for more information
+         fuzzy = { implementation = "lua" },
       },
-
-      opts = function(_, opts)
-         opts.appearance = opts.appearance or {}
-         opts.appearance.kind_icons = vim.tbl_extend("keep", {
-            Color = "██", -- Use block instead of icon for color items to make swatches more usable
-         }, LazyVim.config.icons.kinds)
-      end,
+      opts_extend = { "sources.default" },
    },
 
    {
